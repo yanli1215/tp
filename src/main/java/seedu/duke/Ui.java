bbdfaac39531f88d4000134795c3ce6036ffea08
jalvinchan <chan.jiaxiang728@gmail.com>
2021-03-11 18:17:50 +0800

    Add skeleton code

--- src/main/java/seedu/duke/Ui.java
+++ src/main/java/seedu/duke/Ui.java
@@ -1,4 +1,25 @@
 package seedu.duke;
 
+import java.util.Scanner;
+
 public class Ui {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public String getUserInput() {
+        String input;
+        do {
+            input = scanner.nextLine();
+        } while (input.trim().isEmpty());
+        printDivider();
+        return input;
+    }
+
+    private void printDivider() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public void printFeedback(String feedback) {
+        System.out.println(feedback);
+        printDivider();
+    }
 }
