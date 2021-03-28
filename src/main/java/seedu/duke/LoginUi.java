a1073fda9177d0eeb50bf494cde19706b803fe81
Chihui <NGCH0118@e.ntu.edu.sg>
2021-03-28 21:19:07 +0800

    Remove features to read encrypted password

--- src/main/java/seedu/duke/LoginUi.java
+++ src/main/java/seedu/duke/LoginUi.java
@@ -7,17 +7,11 @@ public class LoginUi extends Ui {
 
     public LoginInfo getLoginInfo() {
         Scanner sc = new Scanner(System.in);
-        //Console con = System.console();
         String userId;
-        char[] passwordCharArray;
-        String passwordString;
-        //String password;
         System.out.println("Enter email address:");
         userId = sc.next();
         System.out.println("Enter password:");
         String password = sc.next();
-        //passwordCharArray = con.readPassword();
-        //passwordString = String.valueOf(passwordCharArray);
         return new LoginInfo(userId, password);
     }
 }
