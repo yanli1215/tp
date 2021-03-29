eb474a35dbe232f10a4bc1b79f9a016481d8a329
Chihui <NGCH0118@e.ntu.edu.sg>
2021-03-30 01:18:01 +0800

    Fix buggy part. When log in user will not be informed if they already have an account. They will only know either user name or password is wrong

--- src/main/java/seedu/duke/login/LoginManager.java
+++ src/main/java/seedu/duke/login/LoginManager.java
@@ -18,18 +18,13 @@ public class LoginManager {
     }
 
     public void verifyLoginInfo(LoginInfo providedLoginInfo) throws WrongLoginInfoException {
-        loginController = new LoginController();
         ArrayList<LoginInfo> loginInfoList = loginInfoFileManager.retrieveLoginInfoList();
         for (LoginInfo loginInfo: loginInfoList) {
             if (loginInfo.equals(providedLoginInfo)) {
                 return;
             }
         }
-        if(!loginController.checkUserIdExists(providedLoginInfo.getUserId())) {
-            System.out.println("You already have account. Please Log in instead!");
-        }else {
-            throw new WrongLoginInfoException();
-        }
+        throw new WrongLoginInfoException();
     }
 
     public void modifyLoginInfo(LoginInfo newLogInfo) {
