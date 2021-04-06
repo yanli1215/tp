63f571264adb5e22c091d57e9d77f35c45a312a6
yanli1215 <yanli1215liu@gmail.com>
2021-04-06 15:20:35 +0800

    fix ./runtest.sh bugs

--- src/main/java/seedu/duke/login/LoginUi.java
+++ src/main/java/seedu/duke/login/LoginUi.java
@@ -59,12 +59,12 @@ public class LoginUi extends Ui {
 
     public void printLoginMenu() {
         System.out.println(super.logo);
-        System.out.println("Select either 1 or 2 or 3 (use numbers): \n"
+        System.out.println("Select either 1 or 2 or 3 (use numbers):\n"
                 + "[Emails address are case sensitive!]\n"
-                + "1. Log In \n"
+                + "1. Log In\n"
                 + "2. Create a new account\n"
                 + "3. Exit");
-        System.out.println("Enter choice: ");
+        System.out.println("Enter choice:");
     }
 
     public LoginInfo getUserInputForLogin() {
